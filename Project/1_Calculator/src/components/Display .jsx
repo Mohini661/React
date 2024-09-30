735e5ff2587929643477888ba1cb67314b373b71
Mohini661 <mohinipatel161098@gmail.com>
2024-09-30 15:06:12 +0530

    Initial commit

--- Project/1_Calculator/src/components/Display .jsx
+++ Project/1_Calculator/src/components/Display .jsx
@@ -1,9 +1,4 @@
-function Display({data,result}) {
-  return (
-    
-      <input type="text" id="inputtext" value={data}></input>
-    
-  );
+function Display({ data, result }) {
+  return <input type="text" id="inputtext" value={data}></input>;
 }
 export default Display;
- 
