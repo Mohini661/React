d3d12d8e49f9c5f580dac37da7af1c5b07e777dc
Mohini661 <mohinipatel161098@gmail.com>
2024-10-09 13:20:14 +0530

    add useEffect in ToDo

--- Project/2_ToDo_App/src/App.jsx
+++ Project/2_ToDo_App/src/App.jsx
@@ -2,13 +2,15 @@ import Title from "./Components/Title";
 import ToDoTask from "./Components/ToDoTask";
 import ToDoItem from "./Components/ToDoItem";
 import { useState } from "react";
+import Demo from "./Components/demo";
+import { toDoListContext } from "./store/hooks";
 
 function App() {
   // const todolist = [{name:"Assignment",date:"10/09/2024"},{name:"Project",date:"11/09/2024"}]
   let [toDoList, setToDoList] = useState([]);
   const addItemHandler = (name, date) => {
     const newArray = [...toDoList, { name: name, date: date }];
-    setToDoList(newArray); 
+    setToDoList(newArray);
   };
 
   const deleteTaskHandler = (name) => {
@@ -17,12 +19,15 @@ function App() {
 
   return (
     <center className="todo-container ">
-      <Title></Title>
-      <ToDoTask onButtonClick={addItemHandler}></ToDoTask>
-      <ToDoItem
-        toDoItemList={toDoList}
-        deleteButtonClick={deleteTaskHandler}
-      ></ToDoItem>
+      <toDoListContext.Provider value={toDoList}>
+        <Title></Title>
+        <ToDoTask onButtonClick={addItemHandler}></ToDoTask>
+        <ToDoItem
+          toDoItemList={toDoList}
+          deleteButtonClick={deleteTaskHandler}
+        ></ToDoItem>
+        <Demo />
+      </toDoListContext.Provider>
     </center>
   );
 }
---
+++ Project/2_ToDo_App/src/Components/Demo.jsx
@@ -0,0 +1,16 @@
+import { useContext } from "react";
+import { toDoListContext } from "../store/hooks";
+
+const Demo = () => {
+  const list = useContext(toDoListContext);
+  return (
+    <div>
+      {/* <h1>Mohini patel</h1> */}
+      {list.map((ele) => (
+        <span>{ele.name}</span>
+      ))}
+    </div>
+  );
+};
+
+export default Demo;
